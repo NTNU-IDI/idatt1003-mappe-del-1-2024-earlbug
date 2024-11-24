ca1517c47e4c6e7be532757333e451270bb100a6
Erlend <erlesu@stud.ntnu.no>
2024-11-24 15:40:30 +0100

    Documentation: remove norwegian todo comments

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -6,27 +6,11 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
 
-    //Starter user interface, og darmed hele programmet
+    // Starts UserInterface, and then also the whole program.
     UserInterface ui = new UserInterface();
     ui.init();
     ui.start();
   }
-
-  //Todo Lære meg streams!! veldig viktig og nyttig
-  //Todo fjerne mengde grocery
-  //finne ut av lambda
-  // string format
-
-  //forbedriger på sikt:
-
-  // Ha et lager av standard/ferdiglagde gorceries, med navn,
-  // så man bare kan velge en grocery neste gang.
-  // Koble måleenheter til navnet til varen.
-  // name, price og measuringunot er allerede utfylt.
-
-  // Find out how to deal with multiple elements with different expiration dates
-
-
 
 
 }
