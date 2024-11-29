ebb93f19cdb9234fbd824f01c62bdb302116046d
Erlend <erlesu@stud.ntnu.no>
2024-11-29 15:50:28 +0100

    feat ParamValidators: add validatePositiveInt

--- src/main/java/edu/ntnu/idi/idatt/utils/ParamValidators.java
+++ src/main/java/edu/ntnu/idi/idatt/utils/ParamValidators.java
@@ -9,16 +9,21 @@ import java.util.Date;
 /**
  * @since 0.1.0
  * @author Erlend Sundsdal
- * @version 0.5.0
+ * @version 0.6.0
  */
 public class ParamValidators {
   static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 
   public static void validatePositiveDouble(double inputFloat) {
-
     if (inputFloat <= 0) {
       throw new IllegalArgumentException("Number must be more than 0, please try again.");
+    }
+  }
+
+  public static void validatePositiveInt(int inpValue) {
+    if (inpValue <= 0) {
+      throw new IllegalArgumentException("The number must be more than 0.");
     }
   }
 
@@ -94,13 +99,13 @@ public class ParamValidators {
       throw new IllegalArgumentException("The grocery is null. Please try again.");
     }
     try {
-      validateString(inpGrocery.getNameOfGrocery());
+      validateString(inpGrocery.getName());
       validatePositiveDouble(inpGrocery.getAmount());
       validateDate(inpGrocery.getExpirationDate());
       validatePositiveDouble(inpGrocery.getPricePerUnit());
       validateString(inpGrocery.getMeasuringUnit());
     } catch (IllegalArgumentException e) {
-      throw new IllegalArgumentException("Grocery is invalid.\n" + e.getMessage());
+      throw new IllegalArgumentException("Grocery is invalid:\n" + e.getMessage());
     }
   }
 }
