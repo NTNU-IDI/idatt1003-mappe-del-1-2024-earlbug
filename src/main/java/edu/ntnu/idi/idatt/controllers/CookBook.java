555e13a737fe9c4757525db8f56a331bcbe5e5af
Erlend <erlesu@stud.ntnu.no>
2024-12-01 16:17:32 +0100

    feat controllers: add complete CookBook class

---
+++ src/main/java/edu/ntnu/idi/idatt/controllers/CookBook.java
@@ -0,0 +1,36 @@
+package edu.ntnu.idi.idatt.controllers;
+
+import edu.ntnu.idi.idatt.models.Grocery;
+import edu.ntnu.idi.idatt.models.Recipe;
+import edu.ntnu.idi.idatt.utils.ParamValidators;
+import java.util.ArrayList;
+
+public class CookBook {
+
+  ArrayList<Recipe> recipeList;
+
+  public CookBook() {
+    recipeList = new ArrayList<>();
+  }
+
+  public void addRecipe(String name, String description, String procedure,
+      ArrayList<Grocery> ingredientList, int portions) {
+    try {
+      ParamValidators.validateString(name);
+      ParamValidators.validateString(description);
+      ParamValidators.validateString(procedure);
+      ParamValidators.validateGroceryArrayList(ingredientList);
+      ParamValidators.validatePositiveInt(portions);
+
+      Recipe newRecipe = new Recipe(name, description, procedure, ingredientList, portions);
+      recipeList.add(newRecipe);
+
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("Recipe could not be added:\n" + e.getMessage());
+    }
+  }
+
+  public ArrayList<Recipe> getRecipeList() {
+    return this.recipeList;
+  }
+}
